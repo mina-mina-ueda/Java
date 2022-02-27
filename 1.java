fdc93ac4837d139ca0f008ce46ae00e2207de16d
mina-mina-ueda <91769651+mina-mina-ueda@users.noreply.github.com>
2022-02-27 13:25:07 +0000

    [Add]

--- 1.java
+++ 1.java
@@ -9,3 +9,22 @@ class Ex1_03_2 {
 		System.out.println("Hello World!! Error is cleared!!" );
 	}
 }
+
+class Ex1_04_1 {
+  public static void main (String[] args) {
+    for(int i = 1 ; i <= 9  ; i++) {
+      String disp3 = "";
+      for(int j = 1 ; j <= 9  ; j++) {
+        disp3 = disp3 + i*j + " ";
+      }
+      if(args.length == 1) {
+        if(i == Integer.parseInt(args[0])){
+          System.out.println(disp3);
+        }
+      }
+      else{
+          System.out.println(disp3);
+      }
+    }
+  }
+}
